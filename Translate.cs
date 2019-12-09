02cf1c0c618c655d28ca6792f4e281f5cd0c75a6
eunrin15 <56423448+eunrin15@users.noreply.github.com>
2019-12-09 11:20:35 +0900

    Create Translate.cs

---
+++ Translate.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace _1002파파고
+{
+    class Translate
+    {
+        #region 프로퍼티
+        public string Result { get; private set; }
+        #endregion
+        #region 파서메서드
+        //파서
+        static public Translate MakeTranslate(XmlNode xn)
+        {
+            string result = string.Empty;
+
+            XmlNode n1 = xn.SelectSingleNode("srcLangType");
+            XmlNode n2 = xn.SelectSingleNode("tarLangType");
+            XmlNode result_node = xn.SelectSingleNode("translatedText");
+            result = ConvertString(result_node.InnerText);
+
+            return new Translate(result);
+        }
+
+        static string ConvertString(string str)
+        {
+            return str;
+        }
+
+        public Translate(string result)
+        {
+            Result = result;
+        }
+        #endregion
+    }
+}
