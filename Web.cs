fb300c03d61bb4f09ed71d55a65eebdce7a00139
eunrin15 <56423448+eunrin15@users.noreply.github.com>
2019-12-09 11:20:53 +0900

    Create Web.cs

---
+++ Web.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _1002파파고
+{
+    public partial class Web : Form
+    {
+        public string Url { get; private set; }
+        public Web()
+        {
+            InitializeComponent();
+        }
+
+        public void LinkSet(string str)
+        {
+            Url = str;
+        }
+        public void LinkLoad()
+        {
+            webBrowser1.Navigate(Url);
+        }
+
+        private void Web_Load(object sender, EventArgs e)
+        {
+            LinkLoad();
+        }
+
+        private void Web_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            this.Dispose();
+        }
+
+        private void Web_FormClosing(object sender, FormClosingEventArgs e)
+        {
+        }
+    }
+}
